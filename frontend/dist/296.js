e0ed4c969e67fcfd6a78b207413b8ff13c418217
justtinCHOI <nicolao011113@gmail.com>
2024-11-15 20:21:58 +0900

    Add RootRedirectPage and NotFoundPage condition

---
+++ frontend/dist/296.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmq_navigator3_0client=self.webpackChunkmq_navigator3_0client||[]).push([[296],{296:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});var i=t(6540),a=t(7767),l=t(5476);const r=function(){var e=(0,l.A)(),n=e.isLogin,t=e.updateUrlAfterLogin,r=e.isInitialLoading;return(0,i.useEffect)((function(){r||n&&t().then()}),[r,n,t]),r?i.createElement("div",null,"Loading..."):n?null:i.createElement(a.C5,{replace:!0,to:"/member/login"})}}}]);
+//# sourceMappingURL=296.js.map
